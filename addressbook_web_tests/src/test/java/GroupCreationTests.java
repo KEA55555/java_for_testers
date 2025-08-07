d90ce2bd2730958a1670c7a54d492ace67581b5d
KEA55555 <kosmoz29@yandex.ru>
2025-08-07 12:59:29 +0300

    Перенесены вспомогательные методы в общий базовый класс TestBase

--- addressbook_web_tests/src/test/java/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -1,40 +1,17 @@
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 public class GroupCreationTests extends TestBase {
 
 
     @Test
     public void canCreateGroup() {
-        if (!isElementPresent(By.name("new"))) {
-            driver.findElement(By.linkText("groups")).click();
-        }
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).sendKeys("group name");
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).sendKeys("group header");
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).sendKeys("group footer");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("group page")).click();
+        openGroupsPage();
+        createGroup("group name", "group header", "group footer");
     }
 
     @Test
     public void canCreateGroupWithEmptyName() {
-        if (!isElementPresent(By.name("new"))) {
-            driver.findElement(By.linkText("groups")).click();
-        }
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).sendKeys("");
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).sendKeys("");
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).sendKeys("");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("group page")).click();
+        openGroupsPage();
+        createGroup("", "", "");
     }
 }
