b9975ca36a1fc9b0cff9d7d2bc21d19b8b38c040
KEA55555 <kosmoz29@yandex.ru>
2025-08-30 16:27:47 +0300

    Задание 13 Создание конфигурационного файла

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Properties;
+
+
 public class ApplicationManager {
 
     protected WebDriver driver;
@@ -15,8 +18,11 @@ public class ApplicationManager {
 
     private ContactHelper contacts;
 
+    private Properties properties;
 
-    public void init(String browser) {
+
+    public void init(String browser, Properties properties) {
+        this.properties = properties;
         if (driver == null) {
             if ("chrome".equals(browser)) {
                 driver = new ChromeDriver();
@@ -26,9 +32,9 @@ public class ApplicationManager {
                 throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
             }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
+            driver.get(properties.getProperty("web.baseUrl"));
             driver.manage().window().setSize(new Dimension(1297, 817));
-            session().login("admin", "secret");
+            session().login(properties.getProperty("web.username"), properties.getProperty("web.password"));
         }
     }
 
